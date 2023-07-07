bc154f484569a2d833c4e131112f48c201323eee
miladesmailpour <milad.esmailpour@gmail.com>
2023-07-07 01:06:06 -0400

    bootstrap and axios added

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const Navbar = ({currentPage, handlePageChange}) =>{
+    return(
+        
+    );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // const root = ReactDOM.createRoot(document.getElementById('root')); // react 18
 const root = document.getElementById("root");
