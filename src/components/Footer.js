f476bb1ebe160e34d65fee09338d7103139e2d49
miladesmailpour <milad.esmailpour@gmail.com>
2023-10-17 23:48:59 -0400

    refactor the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <a href="https://www.linkedin.com/in/milad-esmaeelpour/">
+        <a href="https://www.linkedin.com/in/mila-mailo/">
           <img src="./img/linkedin.png" alt="linkedin" />
         </a>
       </div>
       <div>
-        <a href="https://github.com/miladesmailpour/">
+        <a href="https://github.com/milamailo">
           <img src="./img/github.png" alt="github" />
         </a>
       </div>
       <div>
-        <a href="https://twitter.com/MiladEsmailpour/">
+        <a href="https://twitter.com/mila_mailo">
           <img src="./img/twitter.png" alt="twitter" />
         </a>
       </div>
