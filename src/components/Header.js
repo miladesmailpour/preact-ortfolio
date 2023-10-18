c19441691a35758d8f601ff4cc68c19486625b0d
miladesmailpour <milad.esmailpour@gmail.com>
2023-10-17 22:41:09 -0400

    title added in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img src="./img/avatar.png" alt="avatar"/>
-      <h1>Milad Esmaeelpour</h1>
+      <img src="./img/avatar.png" alt="avatar" />
+      <h1>Mila Mailo</h1>
+      <h6>Full Stack Developer</h6>
     </header>
   );
 };
